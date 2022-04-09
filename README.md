4b42562efd8eab0110415987b126e555014ac9fe
Arash Badie-Modiri <arashbm@gmail.com>
2022-04-10 01:11:48 +0300

    more examples

--- README.md
+++ README.md
@@ -22,7 +22,7 @@ $ pip install dist/dag-X.X.X-XXX.whl
 It might be helpful to use the `--force-reinstall` flag for pip during
 development.
 
-## Basic use
+## Basic examples
 
 Generate a random static network and investigate:
 ```python
@@ -47,4 +47,59 @@ Generate a random static network and investigate:
 >>> g2 = dag.vertiex_induced_subgraph(g, lcc)
 >>> g2
 <dag.undirected_network[dag.int64] with 93 verts and 109 edges>
+```
+A more complete example of static network percolation analysis, running on
+multiple threads, can be found in [`examples/random_network_percolation.py`][perc_examples].
+
+[perc_examples]: examples/random_network_percolation.py
+
+Create a random fully-mixed temporal network and calculate simple
+(unconstrained) reachability from node 0 at time 0 to all nodes and times.
+```python
+>>> import dag
+>>> import dag.temporal_adjacency
+>>> state = dag.mersenne_twister(42)
+>>> g = dag.random_fully_mixed_temporal_network[dag.int64](
+...       size=100, rate=0.01, max_t=1024, random_state=state)
+>>> adj = dag.temporal_adjacency.simple[
+...       dag.undirected_temporal_edge[dag.int64, dag.double]]()
+>>> cluster = dag.out_cluster(
+...       temporal_network=g, temporal_adjacency=adj, vertex=0, time=0.0)
+>>> cluster
+<dag.temporal_cluster[undirected_temporal_edge[int64, double],
+  simple[dag.undirected_temporal_edge[dag.int64, dag.double]]] with volume 100
+  and lifetime (0 1.7976931348623157e+308]>
+>>> cluster.covers(vertex=12, time=100.0)
+True
+>>> # Let's see all intervals where vert 15 is reachable from vert 0 at t=0.0:
+>>> list(cluster.interval_sets()[15])
+[(3.099055278145548, 1.7976931348623157e+308)]
+```
+
+Let's now try limited waiting-time (with dt = 5.0) reachability:
+```python
+>>> import dag
+>>> import dag.temporal_adjacency
+>>> state = dag.mersenne_twister(42)
+>>> g = dag.random_fully_mixed_temporal_network[dag.int64](
+...       size=100, rate=0.01, max_t=1024, random_state=state)
+>>> adj = dag.temporal_adjacency.limited_waiting_time[
+...       dag.undirected_temporal_edge[dag.int64, dag.double]](dt=5.0)
+>>> cluster = dag.out_cluster(
+...       temporal_network=g, temporal_adjacency=adj, vertex=0, time=0.0)
+>>> cluster
+<dag.temporal_cluster[undirected_temporal_edge[int64, double],
+  limited_waiting_time[undirected_temporal_edge[int64, double]]] with volume
+  100 and lifetime (0 1028.9972186553928]>
+>>> cluster.covers(vertex=15, time=16.0)
+True
+>>> list(cluster.interval_sets()[15])
+[(3.099055278145548, 200.17866501023616),
+  (200.39858803326402, 337.96139372380003),
+  ...
+  (1017.5258263596586, 1028.9149586273347)]
+>>> cluster.mass()  # total "human-hours" of reachability
+101747.97444555275
+>>> cluster.lifetime()
+(0.0, 1028.9972186553928)
 ```
---
+++ examples/random_network_percolation.py
@@ -0,0 +1,119 @@
+import concurrent.futures
+import argparse
+import json
+
+import dag
+import tqdm
+import numpy as np
+import matplotlib.pyplot as plt
+
+def cc_analysis(n: int, p: float, seed: int):
+    state = dag.mersenne_twister(seed)
+    g = dag.random_gnp_graph[dag.int64](n, p, state)
+    ccs = [len(cc) for cc in dag.connected_components(g)]
+    largest_comp_size = max(ccs)
+
+    sum_squares = sum([s**2 for s in ccs])
+    sum_cubes = sum([s**3 for s in ccs])
+    nom = sum_squares - largest_comp_size**2
+    denom = n - largest_comp_size
+    classic_chi = nom/denom if denom > 0 else 0.0
+    expected_comp_size = sum_squares/n
+    small_field = 1/n
+    isotropic_chi = 1/small_field * (1/n*sum_squares - 1/(n**2)*sum_cubes)
+    return largest_comp_size, classic_chi, expected_comp_size, isotropic_chi
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+            description="Analysis of connected component size and other "
+            "isometric percolation properties in G(n, p) networks.")
+    parser.add_argument("figure", type=str, help="output figure filename")
+    parser.add_argument("output", type=str, help="output json filename")
+    parser.add_argument("--size", type=int, default=10000, help="system size")
+    parser.add_argument("--ens", type=int, default=100, help="ensemble size")
+    args = parser.parse_args()
+
+    seeds = range(args.ens)
+    size = args.size
+    p_values = np.linspace(0.1/(size-1), 6/(size-1), num=200)
+
+    largest_comps = {}
+    classic_chis = {}
+    expected_comps = {}
+    isotropic_chis = {}
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = []
+        for p in p_values:
+            for seed in seeds:
+                futures.append((p,
+                    executor.submit(cc_analysis, size, p, seed)))
+
+        for p, f in tqdm.tqdm(futures):
+            largest_comp_size, classic_chi,\
+                exp_comp_size, isotropic_chi = f.result()
+
+            if p not in largest_comps:
+                largest_comps[p] = []
+            largest_comps[p].append(largest_comp_size)
+
+            if p not in classic_chis:
+                classic_chis[p] = []
+            classic_chis[p].append(classic_chi)
+
+            if p not in expected_comps:
+                expected_comps[p] = []
+            expected_comps[p].append(exp_comp_size)
+
+            if p not in isotropic_chis:
+                isotropic_chis[p] = []
+            isotropic_chis[p].append(isotropic_chi)
+
+
+    fig, axes = plt.subplots(2, 2, figsize=(8, 8), sharex=True,
+            gridspec_kw={"wspace": 0.3, "hspace": 0.3})
+
+    ax_max, ax_classic_chi, ax_exp, ax_isotropic_chi = axes.flatten()
+
+    ax_max.plot(
+            p_values - 1/(size-1),
+            [np.mean(largest_comps[p]) for p in p_values])
+    ax_max.set_xlabel("$p - p_c$")
+    ax_max.set_ylabel("Largest connected component size")
+
+    ax_classic_chi.plot(
+            p_values - 1/(size-1),
+            [np.mean(classic_chis[p]) for p in p_values])
+    ax_classic_chi.set_xlabel("$p - p_c$")
+    ax_classic_chi.set_ylabel("Susceptibility "
+            "($\\frac{\\sum_{i>0} s_i^2}{\\sum_{i > 0} s_i}$)")
+
+    ax_exp.plot(
+            p_values - 1/(size-1),
+            [np.mean(expected_comps[p]) for p in p_values])
+    ax_exp.set_xlabel("$p - p_c$")
+    ax_exp.set_ylabel("Expected component size")
+
+    ax_isotropic_chi.plot(
+            p_values - 1/(size-1),
+            [np.mean(isotropic_chis[p]) for p in p_values])
+    ax_isotropic_chi.set_xlabel("$p - p_c$")
+    ax_isotropic_chi.set_ylabel("Susceptibility "
+            "($\\sum_i s_i^2 - \\frac{1}{n} \\sum_i s_i^3$)")
+
+    fig.savefig(args.figure)
+
+
+    report = {
+        "size": size,
+        "p_values": list(p_values),
+        "largest_component_size":
+            [np.mean(largest_comps[p]) for p in p_values],
+        "old_school_susceptibility":
+            [np.mean(classic_chis[p]) for p in p_values],
+        "expected_component_size":
+            [np.mean(expected_comps[p]) for p in p_values],
+        "susceptibility":
+            [np.mean(isotropic_chis[p]) for p in p_values]}
+
+    with open(args.output, 'w') as out:
+        json.dump(report, out)
