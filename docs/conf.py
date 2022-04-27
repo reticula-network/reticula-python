7fd54385ed905e518ede6cf43c4d06bf7fec4b8e
Arash Badie Modiri <arashbm@gmail.com>
2022-04-27 23:23:06 +0300

    revert sphinx theme

--- docs/conf.py
+++ docs/conf.py
@@ -66,7 +66,7 @@ bibtex_default_style = 'unsrt'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_book_theme'
+html_theme = 'alabaster'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
