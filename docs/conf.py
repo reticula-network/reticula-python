83264f79b18ef33837bc623db40292b3d2829892
Arash Badie Modiri <arashbm@gmail.com>
2022-05-12 01:45:51 +0300

    add dark code styling

--- docs/conf.py
+++ docs/conf.py
@@ -67,6 +67,9 @@ bibtex_default_style = 'unsrt'
 #
 html_theme = 'furo'
 
+pygments_style = "sphinx"
+pygments_dark_style = "monokai"
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
