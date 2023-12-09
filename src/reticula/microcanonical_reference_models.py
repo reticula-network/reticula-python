f06def0407e3cac913c2796cb8b34870fe730914
Arash Badie-Modiri <arashbm@gmail.com>
2023-12-09 06:28:40 +0100

    fix static shuffling docs and actually import them

--- src/reticula/microcanonical_reference_models.py
+++ src/reticula/microcanonical_reference_models.py
@@ -1,4 +1,6 @@
 from ._reticula_ext.microcanonical_reference_models import (
+        degree_sequence_preserving_shuffling,
+        joint_degree_sequence_preserving_shuffling,
         instant_event_shuffling, link_shuffling, connected_link_shuffling,
         topology_constrained_link_shuffling, timeline_shuffling,
         weight_constrained_timeline_shuffling,
